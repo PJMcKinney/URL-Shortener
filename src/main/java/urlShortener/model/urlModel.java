952348314e923c3350d60d1502d756924c4dafed
PJMcKinney <pj.mckinney@zinkworks.com>
2022-10-17 17:05:12 +0100

    modified pom and app.props to include mysql connection

--- src/main/java/urlShortener/model/urlModel.java
+++ src/main/java/urlShortener/model/urlModel.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "URLs")
+@Table(name = "urls")
 @Getter
 @Setter
 @ToString
--- src/main/java/urlShortener/repository/urlRepository.java
+++ src/main/java/urlShortener/repository/urlRepository.java
@@ -1,22 +1,20 @@
 package urlShortener.repository;
 
-import lombok.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import urlShortener.model.urlModel;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface urlRepository extends JpaRepository<urlModel, UUID> {
 
-    @Query(value = "SELECT longURL from URLs where shortURL = :shortURL", nativeQuery = true)
+    @Query(value = "SELECT longURL from urls where shortURL = :shortURL", nativeQuery = true)
     String findLongURL(@Param("shortURL") String shortURL);
 
-    @Query(value = "SELECT shortURL from URLs where longURL = :longURL", nativeQuery = true)
+    @Query(value = "SELECT shortURL from urls where longURL = :longURL", nativeQuery = true)
     String findShortURL(@Param("longURL") String longURL);
 
 }
